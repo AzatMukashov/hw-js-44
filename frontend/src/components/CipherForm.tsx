fd4ac899c9776312f6d1ce3ff817850119554724
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-12-05 16:14:05 +0600

    настроил эффект для авто скрытия сообщений об ошибках

--- frontend/src/components/CipherForm.tsx
+++ frontend/src/components/CipherForm.tsx
@@ -1,8 +1,18 @@
 import { useAppDispatch, useAppSelector } from '../app/hooks.ts';
+import { useEffect } from 'react';
+import { setError } from '../features/cipherSlice.ts';
 
 const CipherForm = () => {
   const dispatch = useAppDispatch();
   const {password, inputMessage, outputMessage, error} = useAppSelector(state => state.cipher);
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => {
+        dispatch(setError(null));
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [error, dispatch]);
 
   return (
     <div>
