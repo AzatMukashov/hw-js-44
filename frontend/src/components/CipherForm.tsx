e4f9644e996aae79832e7f790b9d7d0d9122182e
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-12-05 17:09:00 +0600

    Рендеринг

--- frontend/src/components/CipherForm.tsx
+++ frontend/src/components/CipherForm.tsx
@@ -1,7 +1,10 @@
 import { useAppDispatch, useAppSelector } from '../app/hooks.ts';
 import { FormEvent, useEffect } from 'react';
-import { setError, setInputMessage, setOutputMessage } from '../features/cipherSlice.ts';
+import { setError, setInputMessage, setOutputMessage, setPassword } from '../features/cipherSlice.ts';
 import axiosAPI from '../axiosApi.ts';
+import { Alert, Container, TextField } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import { ArrowDownward, ArrowUpward } from '@mui/icons-material';
 
 const CipherForm = () => {
   const dispatch = useAppDispatch();
@@ -14,38 +17,72 @@ const CipherForm = () => {
       return () => clearTimeout(timer);
     }
   }, [error, dispatch]);
-const handleEncode = async (e: FormEvent) => {
-  e.preventDefault();
-  if (!password) {
-    dispatch(setError('Password is required'));
-    return;
-  }
-  try {
-    const response = await axiosAPI.post('/encode', {password, message: inputMessage});
-    dispatch(setOutputMessage(response.data.encoded));
-    dispatch(setInputMessage(''));
-  } catch (err) {
-    dispatch(setError('Failed to encode'));
-  }
-};
-const handleDecode = async (e: FormEvent) => {
-  e.preventDefault();
-  if (!password) {
-    dispatch(setError('Password is required'));
-    return;
-  }
-  try {
-    const response = await axiosAPI.post('/decode', {password, message: outputMessage});
-    dispatch(setInputMessage(response.data.decoded));
-    dispatch(setOutputMessage(''));
-  } catch (err) {
-    dispatch(setError('Failed to decode'));
-  }
-};
+  const handleEncode = async (e: FormEvent) => {
+    e.preventDefault();
+    if (!password) {
+      dispatch(setError('Password is required'));
+      return;
+    }
+    try {
+      const response = await axiosAPI.post('/encode', {password, message: inputMessage});
+      dispatch(setOutputMessage(response.data.encoded));
+      dispatch(setInputMessage(''));
+    } catch (err) {
+      dispatch(setError('Failed to encode'));
+    }
+  };
+  const handleDecode = async (e: FormEvent) => {
+    e.preventDefault();
+    if (!password) {
+      dispatch(setError('Password is required'));
+      return;
+    }
+    try {
+      const response = await axiosAPI.post('/decode', {password, message: outputMessage});
+      dispatch(setInputMessage(response.data.decoded));
+      dispatch(setOutputMessage(''));
+    } catch (err) {
+      dispatch(setError('Failed to decode'));
+    }
+  };
   return (
-    <div>
-
-    </div>
+    <Container style={{marginTop: '1rem'}}>
+      <Grid container spacing={2} direction="column">
+        {error ? <Alert severity="error">{error}</Alert> : null}
+        <Grid>
+          <TextField
+            label="Decrypted message"
+            value={inputMessage}
+            onChange={(e) => dispatch(setInputMessage(e.target.value))}
+            fullWidth
+          />
+        </Grid>
+        <Grid container spacing={2} alignItems="center">
+          <Grid size={11}>
+            <TextField
+              label="Password"
+              value={password}
+              onChange={(e) => dispatch(setPassword(e.target.value))}
+              fullWidth
+            />
+          </Grid>
+          <Grid>
+            <ArrowDownward onClick={handleEncode}/>
+          </Grid>
+          <Grid>
+            <ArrowUpward onClick={handleDecode}/>
+          </Grid>
+        </Grid>
+        <Grid>
+          <TextField
+            label="Coded message"
+            value={outputMessage}
+            onChange={(e) => dispatch(setOutputMessage(e.target.value))}
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
